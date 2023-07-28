1e2a8fb665a59e001636c3d15fd1ea87af4d9dd3
KajahMeans <KajahCunningham@gmail.com>
2023-07-27 22:22:38 -0400

    1st data upload

---
+++ Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Pet_Store;
+
+namespace Pet_Store
+{
+    public class Product
+    {
+        public string Name
+        { get; set; }
+        public decimal Price
+        { get; set; }
+        public int Quantity
+        { get; set; }
+        public string Description
+        { get; set; }
+    }
+
+
+    class CatFood : Product
+    {
+        public double WeightPounds
+        { get; set; }
+        public bool KittenFood
+        { get; set; }
+    }
+
+    class DogLeash : Product
+    {
+        public int LengthInches
+        { get; set; }
+        public string Material
+        { get; set; }
+    }
+}
+
+
+
+
+/*
+
+*/
