a483f815306f2859e0331254e2d77c3227a7e602
KajahMeans <KajahCunningham@gmail.com>
2023-07-30 19:32:24 -0400

    Completion of PetStore part 1

--- Product.cs
+++ Product.cs
@@ -1,25 +1,29 @@
 ﻿using System;
-using Pet_Store;
+using System.Runtime.ConstrainedExecution;
+
 
 namespace Pet_Store
+
 {
     public class Product
     {
-        public string Name
-        { get; set; }
-        public decimal Price
-        { get; set; }
-        public int Quantity
-        { get; set; }
-        public string Description
-        { get; set; }
+        //properties
+        public string Name { get; set; /*sets fields in background*/}
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string Description { get; set; }
     }
+    // classes are types; variable have type; line 11 str could make str of catfood
 
 
     class CatFood : Product
     {
         public double WeightPounds
         { get; set; }
+
         public bool KittenFood
         { get; set; }
     }
@@ -28,14 +32,18 @@ namespace Pet_Store
     {
         public int LengthInches
         { get; set; }
+
         public string Material
         { get; set; }
     }
 }
 
+/* Add a product -> create object -> print to console; going to need Parse methods
+Console.Write() - get input
+Console.ReadLine() - add input to object 
+*/
+/* Where does this while loop go?
+ 
 
-
-
-/*
 
 */
--- Program.cs
+++ Program.cs
@@ -1,3 +1,55 @@
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using System.Runtime.Intrinsics.X86;
+using System.Text.Json;
+using Pet_Store;
 
+string userInput = "7";
+
+do
+{
+    Console.WriteLine("Press 1 to add a product");
+    Console.WriteLine("Type ‘exit’ to quit");
+    userInput = Console.ReadLine();
+
+    if (userInput == "1")// userInput = 1 <- assignment
+    {
+        CatFood MeowMix = new CatFood();// new instance of CatFood class
+
+        Console.WriteLine("What is the name of the Cat Food?");// Console.WriteLine () <- requesting the input to print to the screen
+        string name = Console.ReadLine(); // Name is stored in a value; getting input from the user
+        MeowMix.Name = name; //input user provided stored as str Name above,
+
+        Console.WriteLine("How much is the cat food?");
+        decimal price = decimal.Parse(Console.ReadLine());
+        MeowMix.Price = price;
+
+        Console.WriteLine("How many bags of cat food do we have in store?");
+        int quantity = int.Parse(Console.ReadLine());
+        MeowMix.Quantity = quantity;
+
+        Console.WriteLine("What does the bag look like?");
+        string description = Console.ReadLine();
+        MeowMix.Description = description;
+
+        Console.WriteLine("How much does the bag weigh in pounds?");
+        double weight = double.Parse(Console.ReadLine());
+        MeowMix.WeightPounds = weight;
+
+        Console.WriteLine("Is this ok for kittens to eat?");
+        Console.WriteLine("Type 'yes' for yes or 'no' for no.");
+        string KittenEdible = Console.ReadLine();
+        MeowMix.KittenFood = KittenEdible.ToLower() == "yes";// assigning property to result of comparison 
+
+        Console.WriteLine(JsonSerializer.Serialize(MeowMix));
+
+
+    }
+
+
+}
+while (userInput.ToLower() != "exit"); // do between 11-24 until not true
+
+//Console.ReadLine() <- default output is a str
